a22c6cc82cb11725095523df1229f1d73884bf31
Priya <42794888+peacebefore@users.noreply.github.com>
2021-03-25 12:14:40 -0700

    show restaurant details from map screen

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { MapCallout } from "../components/map-callout.component";
 import { Search } from "../components/search.component";
 
 const Map = styled(MapView)`
@@ -46,7 +47,15 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", { restaurant })
+                }
+              >
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
           );
         })}
       </Map>
