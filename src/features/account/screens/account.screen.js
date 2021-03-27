58566c017cef7e930e5c0528b30e3bb799fea4f9
Priya <42794888+peacebefore@users.noreply.github.com>
2021-03-27 15:12:42 -0700

    adding button to login page

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+} from "../components/account.styles";
 
 export const AccountScreen = () => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          color="black"
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          Login
+        </AuthButton>
+      </AccountContainer>
     </AccountBackground>
   );
 };
