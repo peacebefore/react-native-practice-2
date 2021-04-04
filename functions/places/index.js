eae52ed4ac06d1d1692812c6d39c98c19effd55c
Priya <42794888+peacebefore@users.noreply.github.com>
2021-04-03 22:28:02 -0700

    connecting google maps api and update to live photos

--- functions/places/index.js
+++ functions/places/index.js
@@ -1,12 +1,50 @@
 const { mocks, addMockImage } = require("./mock");
 const url = require("url");
+const functions = require("firebase-functions");
 
-module.exports.placesRequest = (request, response) => {
-  const { location } = url.parse(request.url, true).query;
-  const data = mocks[location];
-  if (data) {
-    data.results = data.results.map(addMockImage);
+const addGoogleImage = (restaurant) => {
+  restaurant.photos[0].photo_reference;
+  if (!ref) {
+    restaurant.photos = [
+      "https://www.foodiesfeed.com/free-food-photo/fried-salmon-with-sweet-soy-sauce-in-a-korean-restaurant/",
+    ];
+    return restaurant;
   }
 
-  response.json(data);
+  restaurant.photos = [
+    `https://maps.google.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${
+      functions.config().google.key
+    }`,
+  ];
+  return restaurant;
+};
+
+module.exports.placesRequest = (request, response, client) => {
+  const { location, mock } = url.parse(request.url, true).query;
+  if (mock === "true") {
+    const data = mocks[location];
+    if (data) {
+      data.results = data.results.map(addGoogleImage);
+    }
+
+    return response.json(data);
+  }
+  client
+    .placesNearby({
+      params: {
+        location: location,
+        radius: 1500,
+        type: "restaurant",
+        key: functions.config().google.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      res.data.results = res.data.results.map(addMockImage);
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
