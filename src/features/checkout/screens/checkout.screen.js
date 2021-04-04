06d73778689e8af5896c34347cc8e175babf5ce8
Priya <42794888+peacebefore@users.noreply.github.com>
2021-04-04 16:55:49 -0700

    updating cart UI

--- src/features/checkout/screens/checkout.screen.js
+++ src/features/checkout/screens/checkout.screen.js
@@ -1,18 +1,20 @@
 import React, { useContext } from "react";
-import { Text } from "react-native";
+import { ScrollView } from "react-native";
+import { List } from "react-native-paper";
 
-import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
-
-import { CartIconContainer, CartIcon } from "../components/checkout.styles";
+import { SafeArea } from "../../../components/utility/safe-area.component";
 
 import { CartContext } from "../../../services/cart/cart.context";
 
 import { CreditCardInput } from "../components/credit-card.component";
 
+import { CartIconContainer, CartIcon } from "../components/checkout.styles";
+import { RestaurantInfoCard } from "../../restaurants/components/restaurant-info-card.component";
+
 export const CheckoutScreen = () => {
-  const { cart, restaurant } = useContext(CartContext);
+  const { cart, restaurant, sum } = useContext(CartContext);
 
   if (!cart.length || !restaurant) {
     return (
@@ -24,12 +26,23 @@ export const CheckoutScreen = () => {
       </SafeArea>
     );
   }
-
   return (
     <SafeArea>
-      <Text>{JSON.stringify(cart)}</Text>
-      <Text>restaurant: {JSON.stringify(restaurant)}</Text>
-      <CreditCardInput />
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <Spacer position="left" size="medium">
+          <Spacer position="top" size="large">
+            <Text>Your Order</Text>
+          </Spacer>
+          <List.Section>
+            {cart.map(({ item, price }) => {
+              return <List.Item title={`${item} - ${price / 100}`} />;
+            })}
+          </List.Section>
+          <Text>Total: {sum / 100}</Text>
+        </Spacer>
+        <CreditCardInput />
+      </ScrollView>
     </SafeArea>
   );
 };
